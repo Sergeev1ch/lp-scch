013b671607e177f9eff06b06eba81234772e835e
sergeevich <platonenko.sergsoft@gmail.com>
2021-07-20 11:44:47 +0300

    test

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path =  require('path')
+
+const app = express(); 
+
+app.set('view engine', '.ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'style')));
+app.use(express.static(path.join(__dirname, 'img')));
+app.use(express.static(path.join(__dirname, 'scripts')));
+
+
+const PORT = process.env.PORT || 3000;
+
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+
+app.listen(PORT, () => {
+    console.log('Server has been started...');
+});
