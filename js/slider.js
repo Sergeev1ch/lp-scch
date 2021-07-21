7328c8a7021cd1594354d659eeb62255de91103c
sergeevich <platonenko.sergsoft@gmail.com>
2021-07-21 19:26:23 +0300

    test

--- js/slider.js
+++ js/slider.js
@@ -3,6 +3,14 @@ $(document).ready(function(){
         center: true,
         items: 3,
         loop: true,
-        margin: 10
+        margin: 10,
+        responsive: {
+        	0: {
+        		items: 1
+        	},
+        	768:{
+        		items: 3
+        	}
+        }
     });
 });
